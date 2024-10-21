6493b6e5e53a07130c98354f4aafa5bf90e5e463
lashameskhidze <lashameskhidze0@gmail.com>
2024-10-21 11:50:25 -0700

    script

---
+++ script.js
@@ -0,0 +1,41 @@
+let num = 18;
+let square = num * num;
+
+if (square > 100) {
+  console.log("მეტია ასზე");
+} else {
+  console.log("ნაკლებია ასზე");
+}
+
+let number = 80; 
+
+if (number > 50 && number < 100) {
+    console.log(" ნაკლებია");
+} else {
+    console.log("მეტია");
+}
+
+// 
+let Number = -3; 
+
+if (number <= 0) {
+    console.log("რიცხვი ნულის ტოლი ან უარყოფითია");
+} else {
+    console.log("რიცხვი უარყოფითია");
+}
+
+
+
+// 
+let length = 8;  
+let width = 6;   
+
+let perimeter = 2 * (length + width);
+
+
+if (perimeter > 50) {
+    console.log("პერიმეტრი 50ზე მეტია");
+} else {
+    console.log("პერიმეტრი 50ზე ნაკლებია");
+}
+
